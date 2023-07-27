44f0e98914e8ed0a0fbfda9b0c5abdaf20eff05c
Bronstein0x113c1c3 <tuananhsmart123456789@gmail.com>
2023-07-27 21:22:28 +0700

    Date.h v1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <SDL2/SDL.h>
 #include <UserLib/test.h>
 #include <UserLib/Time.h>
+#include <UserLib/Date.h>
 #include <vector>
 #include <iostream>
+#include <bits/stdc++.h>
 
+
+using namespace std;
 int main(int argc, char *argv[])
 {
     SDL_Init(SDL_INIT_EVERYTHING);
@@ -13,5 +17,10 @@ int main(int argc, char *argv[])
     std::cout << time << std::endl;
 
     print_hello_world();
-    return 0;
-}
+    
+    cout << "sdfsdfewrefdsbfdfsefewfsdgdfvwfeqfsefvdfvb" << endl;
+
+    Date d1{"-1/9/2002"};
+    cout<<d1<<endl;
+    // cout<<t.tm_hour<<endl<<t.tm_mon<<endl<<t.tm_wday<<endl<<t.tm_yday<<endl<<t.tm_year;
+ }
--- src/include/UserLib/Date.h
+++ src/include/UserLib/Date.h
@@ -1,6 +1,77 @@
-struct Date
+#include <bits/stdc++.h>
+
+using namespace std;
+bool validation_through_lib(string date);
+bool validation_with_stream(string date);
+class Date
 {
+private:
     int day;
     int month;
     int year;
+
+public:
+    Date(int day = 0, int month = 0, int year = 9999)
+    {
+        this->day = day;
+        this->month = month;
+        this->year = year;
+    }
+    Date(string date)
+    {
+        int day = 0, month = 0, year = 9999;
+        if (validation_through_lib(date) && validation_with_stream(date))
+        {
+            stringstream ss(date);
+            char delimiter;
+            ss >> day >> delimiter >> month >> delimiter >> year;
+            this->day = day;
+            this->month= month;
+            this->year = year;
+        }
+            this->day = day;
+            this->month= month;
+            this->year = year;
+    }
+    friend std::ostream &operator<<(std::ostream &os, const Date &date)
+    {
+        os << date.day << "/" << date.month << "/" << date.year;
+        return os;
+    }
 };
+Date parsing(string date)
+{
+    int day = 0, month = 0, year = 9999;
+    if (validation_through_lib(date) && validation_with_stream(date))
+    {
+        stringstream ss(date);
+        char delimiter;
+        ss >> day >> delimiter >> month >> delimiter >> year;
+        return Date(day, month, year);
+    }
+    return Date(day, month, year);
+}
+bool validation_through_lib(string date)
+{
+    stringstream ss(date);
+    tm time{};
+    ss >> get_time(&time, "%d/%m/%y");
+    if (ss.fail())
+    {
+        cout << "Failed to parse date and time\n";
+        return false;
+    }
+    return true;
+}
+bool validation_with_stream(string date)
+{
+    stringstream ss(date);
+    int day, month, year;
+    char delimiter;
+    ss >> day >> delimiter >> month >> delimiter >> year;
+    if ((month == 2) && (day >= 30))
+    {
+        return false;
+    }
+    return true;
+}
