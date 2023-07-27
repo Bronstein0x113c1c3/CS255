371f95ef23932065797f50b30507fa6a5c5b8336
Bronstein0x113c1c3 <tuananhsmart123456789@gmail.com>
2023-07-27 21:41:39 +0700

    Date.h v1.0.1

--- src/include/UserLib/Date.h
+++ src/include/UserLib/Date.h
@@ -3,6 +3,7 @@
 using namespace std;
 bool validation_through_lib(string date);
 bool validation_with_stream(string date);
+bool validation(int a, int b, int c);
 class Date
 {
 private:
@@ -13,10 +14,20 @@ private:
 public:
     Date(int day = 0, int month = 0, int year = 9999)
     {
-        this->day = day;
-        this->month = month;
-        this->year = year;
+        if (validation(day, month, year))
+        {
+            this->day = day;
+            this->month = month;
+            this->year = year;
+        }
+        else
+        {
+            this->day = 0;
+            this->month = 0;
+            this->year = 9999;
+        }
     }
+
     Date(string date)
     {
         int day = 0, month = 0, year = 9999;
@@ -26,12 +37,12 @@ public:
             char delimiter;
             ss >> day >> delimiter >> month >> delimiter >> year;
             this->day = day;
-            this->month= month;
+            this->month = month;
             this->year = year;
         }
-            this->day = day;
-            this->month= month;
-            this->year = year;
+        this->day = day;
+        this->month = month;
+        this->year = year;
     }
     friend std::ostream &operator<<(std::ostream &os, const Date &date)
     {
@@ -70,6 +81,18 @@ bool validation_with_stream(string date)
     char delimiter;
     ss >> day >> delimiter >> month >> delimiter >> year;
     if ((month == 2) && (day >= 30))
+    {
+        return false;
+    }
+    return true;
+}
+bool validation(int a, int b, int c)
+{
+    if (a < 0 || b < 0 || c < 0 || a >= 32 || b >= 13)
+    {
+        return false;
+    }
+    if (b == 2 || a >= 30)
     {
         return false;
     }
