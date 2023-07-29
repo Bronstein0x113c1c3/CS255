68f143be8aeed00a400b450feaad3a43daca0400
Sad <nguyentrungson217@gmail.com>
2023-07-30 04:30:26 +0700

    Big Change In All Files

--- src/include/UserLib/Human.h
+++ src/include/UserLib/Human.h
@@ -7,7 +7,6 @@
 #include "Date.h"
 #include "Record.h"
 
-using namespace std;
 class Human
 {
 private:
@@ -43,37 +42,37 @@ public:
     };
     void addRecord(Queue<Record> record);
 
-    //PROPERTIES
+    // PROPERTIES
     int getID() { return this->ID; };
-    string getLastMidName() const { return this->last_mid_name; };
-    string getFirstName() const { return this->first_name; };
-    string getDepartment() const { return this->department; };
-    string getPosition() const { return this->position; };
+    std::string getLastMidName() const { return this->last_mid_name; };
+    std::string getFirstName() const { return this->first_name; };
+    std::string getDepartment() const { return this->department; };
+    std::string getPosition() const { return this->position; };
     Date getDateOfBirth() const { return this->date_of_birth; };
-    string getBirthPlace() const { return this->birth_place; };
-    string getEmail() const { return this->email; };
-    string getPhoneNum() const { return this->phone_num; };
+    std::string getBirthPlace() const { return this->birth_place; };
+    std::string getEmail() const { return this->email; };
+    std::string getPhoneNum() const { return this->phone_num; };
     Date getFirstDayAtWork() const { return this->first_day_at_work; };
 
-    void setLastMidName(string last_mid_name) { this->last_mid_name = last_mid_name; };
-    void setFirstName(string first_name) { this->first_name = first_name; };
-    void setDepartment(string department) { this->department = department; };
-    void setPosition(string position) { this->position = position; };
+    void setLastMidName(std::string last_mid_name) { this->last_mid_name = last_mid_name; };
+    void setFirstName(std::string first_name) { this->first_name = first_name; };
+    void setDepartment(std::string department) { this->department = department; };
+    void setPosition(std::string position) { this->position = position; };
     void setDateOfBirth(Date date_of_birth) { this->date_of_birth = date_of_birth; };
-    void setBirthPlace(string birth_place) { this->birth_place = birth_place; };
-    void setEmail(string email) { this->email = email; };
-    void setPhoneNum(string phone_num) { this->phone_num = phone_num; };
+    void setBirthPlace(std::string birth_place) { this->birth_place = birth_place; };
+    void setEmail(std::string email) { this->email = email; };
+    void setPhoneNum(std::string phone_num) { this->phone_num = phone_num; };
     void setFirstDayAtWork(Date first_day_at_work) { this->first_day_at_work = first_day_at_work; };
     Queue<Record> getDaysWork() const { return this->days_work; };
 
     // METHODS
-    void addDaysWork(Record new_record) 
-    { 
-        this->days_work.Enqueue(new_record); 
+    void addDaysWork(Record new_record)
+    {
+        this->days_work.Enqueue(new_record);
     }
 };
 
-Human::Human(int ID, string last_mid_name, string first_name, string department, string position, string dob, string birth_place, string email, string phone_num, string first_day_at_work)
+Human::Human(int ID, std::string last_mid_name, std::string first_name, std::string department, std::string position, std::string dob, std::string birth_place, std::string email, std::string phone_num, std::string first_day_at_work)
 {
     this->ID = ID;
     this->last_mid_name = last_mid_name;
@@ -94,6 +93,26 @@ Human::~Human()
 {
 }
 
-// class Employee:Human
+class Employee : public Human
+{
+};
+class DeputyManager : public Human
+{
+};
+class Manager : public Human
+{
+};
+class ViceDirector : public Human
+{
+};
+class Director : public Human
+{
+};
+class VicePresident : public Human
+{
+};
+class President : public Human
+{
+};
 
 #endif
