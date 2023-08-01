10eb4f4a85a77697d7fc7ed4dfb59ebbe9cd928e
Sad <nguyentrungson217@gmail.com>
2023-08-02 02:23:57 +0700

    Adjust Corporation.h

--- src/include/UserLib/Corporation.h
+++ src/include/UserLib/Corporation.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include "Human.h"
 #include "../DSA/Queue.h"
+#include "../DSA/HashMap.h"
 #include "Company.h"
 class Corporation
 {
@@ -11,12 +12,12 @@ private:
     std::string name = "BKCorporation";
     President president = President();
     Queue<VicePresident> vice_president;
-    Queue<Company> company_list;
+    HashMap<std::string, Company> company_list;
 
 public:
     // CONSTRUCTORS
     Corporation(){};
-    Corporation(std::string name, President president, Queue<VicePresident> vice_president, Queue<Company> company);
+    Corporation(std::string name, President president, Queue<VicePresident> vice_president, HashMap<std::string, Company> company);
     ~Corporation();
 
     // OPERATORS
@@ -28,29 +29,31 @@ public:
            << corporation.company_list << std::endl;
         return os;
     }
-    bool operator==(const Corporation& otherCorporation){
-        return this->name==otherCorporation.name;
+    bool operator==(const Corporation &otherCorporation)
+    {
+        return this->name == otherCorporation.name;
     }
-    bool operator!=(const Corporation& otherCorporation){
-        return this->name!=otherCorporation.name;
+    bool operator!=(const Corporation &otherCorporation)
+    {
+        return this->name != otherCorporation.name;
     }
     // PROPERTIES
     std::string getName() { return this->name; };
     President getPresident() { return this->president; };
     Queue<VicePresident> getVicePresidentList() { return this->vice_president; };
-    Queue<Company> getCompanyList() { return this->company_list; };
+    HashMap<std::string, Company> getCompanyList() { return this->company_list; };
 
     void setName(std::string name) { this->name = name; };
     void setPresident(President president) { this->president = president; };
     void setVicePresidentList(const Queue<VicePresident> &vice_president_list) { this->vice_president = vice_president_list; };
-    void setCompanyList(const Queue<Company> &company_list) { this->company_list = company_list; };
+    void setCompanyList(const HashMap<std::string, Company> &company_list) { this->company_list = company_list; };
 
     // METHODS
     void addVicePresident(VicePresident vice_president) { this->vice_president.Enqueue(vice_president); };
-    void addCompany(Company company) { this->company_list.Enqueue(company); };
+    void addCompany(Company company) { this->company_list.assign(company.getName(), company); };
 };
 
-Corporation::Corporation(std::string name, President president, Queue<VicePresident> vice_president, Queue<Company> company)
+Corporation::Corporation(std::string name, President president, Queue<VicePresident> vice_president, HashMap<std::string, Company> company)
 {
     this->name = name;
     this->president = president;
