1e3777b95fe267ac9830aadcb5a8f1c08882ba8b
Sad <nguyentrungson217@gmail.com>
2023-08-01 10:54:07 +0700

    Regex Time

---
+++ src/include/UserLib/ValidateRegex.h
@@ -0,0 +1,15 @@
+#ifndef VALIDATEREGEX_H
+#define VALIDATEREGEX_H
+
+#include <string>
+
+bool validateID(std::string ID);
+bool validateName(std::string name);
+bool validateDepartmentName(std::string department_name);
+bool validatePosition(std::string posistion_name);
+bool validateDate(std::string date);
+bool validateBirthPlace(std::string birth_place);
+bool validateEmail(std::string email);
+bool validatePhoneNum(std::string phone_num);
+
+#endif
