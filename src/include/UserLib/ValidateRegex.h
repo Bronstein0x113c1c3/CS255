43948dbace402ac431dcdeb17887b63005652e0a
Sad <nguyentrungson217@gmail.com>
2023-08-01 13:34:02 +0700

    Regex Time

--- src/include/UserLib/ValidateRegex.h
+++ src/include/UserLib/ValidateRegex.h
@@ -14,34 +14,38 @@ bool validateBirthPlace(std::string birth_place);
 bool validateEmail(std::string email);
 bool validatePhoneNum(std::string phone_num);
 
-bool validateName(std::string name){
-    return (std::regex_match(name,std::regex("(([A-Z]{1}[a-z]+)\\s?){1,}")));
+bool validateName(std::string name)
+{
+    return (std::regex_match(name, std::regex("(([A-Z]{1}[a-z]+)\\s?){1,}")));
 }
-bool validateID(std::string ID){
-    return std::regex_match(ID,std::regex("([0-9])+"));
+bool validateID(std::string ID)
+{
+    return std::regex_match(ID, std::regex("([0-9])+"));
 }
-bool validateDepartmentName(std::string department_name){
-    return std::regex_match(department_name,std::regex("(([A-Z]{3})([a-z]+))"));
+bool validateDepartmentName(std::string department_name)
+{
+    return std::regex_match(department_name, std::regex("(([A-Z]{3})([a-z]+))"));
 }
-bool validatePosition(std::string posistion_name){
-    return std::regex_match(posistion_name,std::regex("(([A-Z]{1}[a-z]+)\\s?){1,}"));
+bool validatePosition(std::string posistion_name)
+{
+    return std::regex_match(posistion_name, std::regex("(([A-Z]{1}[a-z]+)?){1,}"));
 }
 
 bool validateDate(std::string date)
 {
-    std::regex date_pattern("(((0[1-9]|[12]\\d|3[01])\\/(0[13578]|1[02])\\/((19|[2-9]\\d)\\d{2}))|((0[1-9]|[12]\\d|30)\\/(0[13456789]|1[012])\\/((19|[2-9]\\d)\\d{2}))|((0[1-9]|1\\d|2[0-8])\\/02\\/((19|[2-9]\\d)\\d{2}))|(29\\/02\\/((1[6-9]|[2-9]\\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))");
+    std::regex date_pattern("*(((0[1-9]|[12]\\d|3[01])\\/(0[13578]|1[02])\\/((19|[2-9]\\d)\\d{2}))|((0[1-9]|[12]\\d|30)\\/(0[13456789]|1[012])\\/((19|[2-9]\\d)\\d{2}))|((0[1-9]|1\\d|2[0-8])\\/02\\/((19|[2-9]\\d)\\d{2}))|(29\\/02\\/((1[6-9]|[2-9]\\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))");
     return std::regex_match(date, date_pattern);
 }
 
 bool validateBirthPlace(std::string birth_place)
 {
     std::regex place_pattern("[A-Z]{1}[a-z]+");
-    return std::regex_match(birth_place, place_pattern);   
+    return std::regex_match(birth_place, place_pattern);
 }
 
 bool validateEmail(std::string email)
 {
-    std::regex email_pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$");
+    std::regex email_pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}");
     return std::regex_match(email, email_pattern);
 }
 
