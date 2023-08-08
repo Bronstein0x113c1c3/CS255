b738dbf2a9ca5710728960c225b6b1c949fc6f0f
Sad <nguyentrungson217@gmail.com>
2023-08-08 10:02:07 +0700

    GUI CS255

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-#include <SDL2/SDL.h>
+#include <SDL2/SDL.h>   // FOR GUI
+#include <mysql.h>      // FOR DATABASE
 #include <UserInteractions/UserInteractions.h>
 
 int main(int argc, char *argv[])
@@ -7,5 +8,6 @@ int main(int argc, char *argv[])
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Quit();
 
+    MYSQL* conn = nullptr;
     return 0;
 }
